d926b9a941eb0503a6acb1ee24ddc047538b6647
vincenzocavallaro <vincenzo.cavallaro98@gmail.com>
2026-01-23 00:52:13 +0100

    Fase 2-"The binary world"

--- src/Main.java
+++ src/Main.java
@@ -9,10 +9,24 @@ public class Main {
         System.out.println("Input String:");
         String text = scanner.nextLine();
 
+        System.out.println("the Result:");
         String[] result = logic.tabellar(text);
-
         for (String s : result) {
-            System.out.print(s + " ");
+            char c = s.charAt(0);
+            int ascii = c;
+
+            String bin = Integer.toBinaryString(ascii);
+
+            while (bin.length() < 7) {
+                bin = "0" + bin;
+            }
+
+            System.out.println(s + " = " + bin);
         }
+
     }
 }
+
+
+
+
